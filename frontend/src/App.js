421cdae808f5fe66753d9a56302a1dba3bc57958
Exequiel <exequiel.hashimoto@outlook.com>
2022-06-24 22:34:03 -0700

    home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import SignupForm from "./components/SignUpFormModal/SignUpForm";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
+import Home from './components/home';
 
 function App() {
   const dispatch = useDispatch();
@@ -17,6 +18,9 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
           <Route path="/signup">
             <SignupForm />
           </Route>
---
+++ frontend/src/components/home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <h1>Smelp</h1>
+        </div>
+    )
+};
+
+export default Home;
