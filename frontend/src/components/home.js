b81e4f708fb4a5617b029dbc34855a465b457dd3
Exequiel <exequiel.hashimoto@outlook.com>
2022-06-28 08:33:35 -0700

    db done, demo user done

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,27 +1,29 @@
-import * as sessionActions from "../store/session";
-import { useDispatch } from "react-redux";
+// import * as sessionActions from "../store/session";
+// import { useDispatch } from "react-redux";
+// import home.css
 
 function Home() {
+//   const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+//   const demo = (e) => {
+//     e.preventDefault();
+//     const demoAuth = "Demo-lition";
+//     const demoPassword = "password";
+//     return dispatch(
+//       sessionActions.login({
+//         credential: demoAuth,
+//         password: demoPassword,
+//       })
+//     );
+//   };
 
-    const demo = (e) => {
-        e.preventDefault();
-        const demoAuth = "Demo-lition";
-        const demoPassword = "password";
-        return dispatch(
-          sessionActions.login({
-            credential: demoAuth,
-            password: demoPassword,
-          })
-        );
-      };
-    return (
-        <div>
-            <h1>Smelp</h1>
-            <button onClick={demo} className="demo-login" type="submit">Take a look!</button>
-        </div>
-    )
-};
+//   console.log(sessionActions);
+
+  return (
+    <div>
+      <h1>Smelp</h1>
+    </div>
+  );
+}
 
 export default Home;
